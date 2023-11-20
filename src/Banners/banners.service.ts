15a45e1e72d2a3915de9a0d5f804b2c306e8cbfa
ido elishar <idoelishar81@gmail.com>
2023-11-20 20:40:45 +0200

    add await to functions

--- src/Banners/banners.service.ts
+++ src/Banners/banners.service.ts
@@ -1,15 +1,25 @@
 import { BannerModel, Banner } from './Banners.model';
 
 const bannerService = {
-    getAllBanners: async (): Promise<Banner[]> => BannerModel.find({}),
-    getBannerById: async (_id: string): Promise<Banner | null> => BannerModel.findOne({ _id }),
-    createBanner: async (banner: Banner): Promise<Banner> => BannerModel.create(banner),
-    updateBanner: async (_id: string, updatedBanner: Banner): Promise<Banner | null> =>
-        BannerModel.findOneAndUpdate({ _id }, updatedBanner, { new: true }),
-    deleteBanner: async (_id: string): Promise<Banner | null> => BannerModel.findOneAndDelete({ _id }),
+    getAllBanners: async (): Promise<Banner[]> => {
+        return await BannerModel.find({});
+    },
+    getBannerById: async (_id: string): Promise<Banner | null> => {
+        return await BannerModel.findOne({ _id });
+    },
+    createBanner: async (banner: Banner): Promise<Banner> => {
+        return await BannerModel.create(banner);
+    },
+    updateBanner: async (_id: string, updatedBanner: Banner): Promise<Banner | null> => {
+        return await BannerModel.findOneAndUpdate({ _id }, updatedBanner, { new: true });
+    },
+    deleteBanner: async (_id: string): Promise<Banner | null> => {
+        return await BannerModel.findOneAndDelete({ _id });
+    },
     getBannersByCategory: async (category: string): Promise<Banner[]> => {
         return await BannerModel.find({ category });
     },
 };
+
 
 export default bannerService;
