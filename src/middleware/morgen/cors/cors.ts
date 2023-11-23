a3a9d4dfe7b471fe091c8d190d7a34ba8e2a63e2
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-23 15:13:20 +0200

    start

--- src/middleware/morgen/cors/cors.ts
+++ src/middleware/morgen/cors/cors.ts
@@ -1,14 +1,7 @@
 import cors, { CorsOptions } from "cors";
 
-const whiteList = [
-  'http://127.0.0.1:5173',
-  'http://localhost:5175',
-  'http://localhost:5173',
-  "http://localhost:8008"
-];
-
 const corsOptions: CorsOptions = {
-  origin: whiteList,
+  origin:"*",
 };
 
 const corsHandler = cors(corsOptions);
