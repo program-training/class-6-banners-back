8eeb07715fb3f52bf1460c4891a0f590c8b96a4f
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-30 13:50:53 +0200

    api

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(morgan)
 const port = process.env.PORT || 8008
 app.listen(port, () => console.log(`server run in port ${port}!`));
 
-app.use('/banner/api/banners', productRoute);
-app.use('/banner/api/users', usersRoute);
+app.use('/banners', productRoute);
+app.use('/users', usersRoute);
 
 
