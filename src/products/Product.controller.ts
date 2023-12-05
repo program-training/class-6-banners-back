6405c42b695a954fede17969b662806a25d9e7ab
ido elishar <idoelishar81@gmail.com>
2023-12-05 11:52:30 +0200

    get products

--- src/products/Product.controller.ts
+++ src/products/Product.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import axios from 'axios'
+const STORE_SERVER = process.env.STORE_SERVER || "https://store-back-3.onrender.com";
+
 const getAllBanners = async (req: Request, res: Response): Promise<void> => {
     try {
         console.log('Request received to get all products');
-        const response = await axios.get('https://store-back-3.onrender.com/products');
+        const response = await axios.get((`${STORE_SERVER}/products`));
         const products = response.data;
         res.status(200).json(products);
     } catch (error) {
