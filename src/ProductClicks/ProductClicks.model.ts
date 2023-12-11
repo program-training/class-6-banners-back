2f6febfc4b9ec2d43e9bb3e29a67910d17c4563a
ido elishar <idoelishar81@gmail.com>
2023-12-11 20:11:30 +0200

    fix server

--- src/ProductClicks/ProductClicks.model.ts
+++ src/ProductClicks/ProductClicks.model.ts
@@ -1,21 +1,38 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import Joi from 'joi';
 
-export interface ProductClicks extends Document {
-    banner_id: string;
-    clicks: Record<string, number>;
-}
-
-const productClicksSchema = new Schema<ProductClicks>({
-    banner_id: { type: String, required: true },
-    clicks: { type: Map, of: Number }
-}, { versionKey: false });
-
-const ProductClicksModel = mongoose.model<ProductClicks>('dateforbanner', productClicksSchema, 'dateforbanner');
+const productClickJoiSchema = Joi.object({
+    date: Joi.string().required(),
+    count: Joi.number().min(0).required()
+});
 
 const productClicksJoiSchema = Joi.object({
     banner_id: Joi.string().required(),
-    clicks: Joi.object().pattern(Joi.date().iso(), Joi.number().min(0)).required()
+    clicks: Joi.array().items(productClickJoiSchema).required()
 });
 
-export { ProductClicksModel, productClicksJoiSchema };
+export { productClicksJoiSchema };
+
+export interface ProductClick extends Document {
+    date: string;
+    count: number;
+}
+
+export interface ProductClicks extends Document {
+    banner_id: string;
+    clicks: ProductClick[];
+}
+
+const productClickSchema = new Schema<ProductClick>({
+    date: { type: String, required: true },
+    count: { type: Number, required: true }
+}, { _id: false });
+
+const productClicksSchema = new Schema<ProductClicks>({
+    banner_id: { type: String, required: true },
+    clicks: [productClickSchema]
+}, { versionKey: false });
+
+const ProductClicksModel = mongoose.model<ProductClicks>('ProductClicks', productClicksSchema, 'dateforbanner');
+
+export { ProductClicksModel };
